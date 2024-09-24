91d086973c9e630c354f4b611654937cad6d2b1e
Pity650 <141268368+Pity650@users.noreply.github.com>
2024-09-24 11:54:24 -0300

    marina e muitolegal(mentira)

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ console.log(soma(2,3));
 function área (number1,number2) {
     return number1*number2;
 }
+let ariatrapezio= function (number4){
+    return number1+number2*number3/number4
+}
+console.log (trapezio)
